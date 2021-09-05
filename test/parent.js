9132d3fc5e26b92b3c523e52092e1e7d9f3ccaad
Helio Machado <0x2b3bfa0+git@googlemail.com>
2021-09-05 17:42:44 +0200

    Remove SIGINT signal handler from parent process

--- test/parent.js
+++ test/parent.js
@@ -1,7 +1,3 @@
-const path = require("path");
-
-process.addListener("SIGTERM", () => console.log("PARENT: SIGTERM"));
-
 require("..")
-  .pseudoexec("node", [path.join(__dirname, "child.js")])
+  .pseudoexec("node", [require("path").join(__dirname, "child.js")])
   .then((code) => console.log(`PARENT: CHILD EXIT CODE ${code}`));
